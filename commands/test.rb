3ff26b268dc89dad264d498496dc59c6cf00eadf
Andrew Shebanow <ashebanow@gmail.com>
2009-11-19 14:21:20 -0800

    convert rails.tmbundle's test-related commands to ruby

---
+++ commands/test.rb
@@ -0,0 +1,35 @@
+require 'radrails'
+
+with_defaults { :scope => "source.ruby.rails, source.yaml" ],
+                :key_binding => [ :control, "\\" ],
+                :output => :show_as_html }
+do |bundle|
+  command "Test All" do |cmd|
+    cmd.invoke = "rake test"
+  end
+
+  command "Test Functionals" do |cmd|
+    cmd.invoke = "rake test:functionals"
+  end
+
+  command "Test Integration" do |cmd|
+    cmd.invoke = "rake test:integration"
+  end
+
+  command "Test Plugins" do |cmd|
+    cmd.invoke = "rake test:plugins"
+  end
+
+  command "Test Recent" do |cmd|
+    cmd.invoke = "rake test:recent"
+  end
+
+  command "Test Uncommitted" do |cmd|
+    cmd.invoke = "rake test:uncommitted"
+  end
+
+  command "Test Units" do |cmd|
+    cmd.invoke = "rake test:units"
+  end
+
+end
