71815e3de21c3007d5730da07c1c3414d743887e
Andrew Shebanow <ashebanow@gmail.com>
2009-11-23 13:46:20 -0800

    add ruby versions of intelligent migration snippets, first cut

--- Support/bin/intelligent_migration_snippet.rb
+++ Support/bin/intelligent_migration_snippet.rb
@@ -6,112 +6,120 @@
 # Inserts a migration snippet into 2 places in the document, one
 # piece in self.up and one in self.down.
 
-snippets = {
-  'rename_column' =>
-    { :up   => 'rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}$0',
-      :down => 'rename_column :$1, :$3, :$2' },
+module IntelligentMigration
 
-  'rename_column_continue' =>
-    { :up   => 'rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}
-mncc$0',
-      :down => 'rename_column :$1, :$3, :$2' },
+  @@snippets = {
+    'rename_column' =>
+      { :up   => 'rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}$0',
+        :down => 'rename_column :$1, :$3, :$2' },
 
-  'rename_table' =>
-    { :up   => 'rename_table :${1:old_table_name}, :${2:new_table_name}$0',
-      :down => 'rename_table :$2, :$1' },
+    'rename_column_continue' =>
+      { :up   => 'rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}
+  mncc$0',
+        :down => 'rename_column :$1, :$3, :$2' },
 
-  'rename_table_continue' =>
-    { :up   => 'rename_table :${1:old_table_name}, :${2:new_table_name}
-mntc$0',
-      :down => 'rename_table :$2, :$1' },
+    'rename_table' =>
+      { :up   => 'rename_table :${1:old_table_name}, :${2:new_table_name}$0',
+        :down => 'rename_table :$2, :$1' },
+
+    'rename_table_continue' =>
+      { :up   => 'rename_table :${1:old_table_name}, :${2:new_table_name}
+  mntc$0',
+        :down => 'rename_table :$2, :$1' },
   
-  'change_column' =>
-    { :up   => 'change_column :${1:table_name}, :${2:column_name}, :${4:string}$5',
-      :down => 'change_column :$1, :$2, :${6:string}$7' },
+    'change_column' =>
+      { :up   => 'change_column :${1:table_name}, :${2:column_name}, :${4:string}$5',
+        :down => 'change_column :$1, :$2, :${6:string}$7' },
   
-  'change_column_default' =>
-    { :up   => 'change_column_default :${1:table_name}, :${2:column_name}, ${4:"${5:new default}"}',
-      :down => 'change_column_default :$1, :$2, ${6:"${7:old default}"}' },
+    'change_column_default' =>
+      { :up   => 'change_column_default :${1:table_name}, :${2:column_name}, ${4:"${5:new default}"}',
+        :down => 'change_column_default :$1, :$2, ${6:"${7:old default}"}' },
 
-  'add_remove_column' =>
-    { :up   => 'add_column :${1:table_name}, :${2:column_name}, :${3:string}$0',
-      :down => 'remove_column :$1, :$2' },
+    'add_remove_column' =>
+      { :up   => 'add_column :${1:table_name}, :${2:column_name}, :${3:string}$0',
+        :down => 'remove_column :$1, :$2' },
 
-  'add_remove_column_continue' =>
-    { :up   => 'add_column :${1:table_name}, :${2:column_name}, :${3:string}
-marcc$0',
-      :down => 'remove_column :$1, :$2' },
+    'add_remove_column_continue' =>
+      { :up   => 'add_column :${1:table_name}, :${2:column_name}, :${3:string}
+  marcc$0',
+        :down => 'remove_column :$1, :$2' },
 
-  'add_remove_timestamps' =>
-    { :up   => 'add_timestamps :${1:table_name}$0',
-      :down => 'remove_timestamps :$1' },
+    'add_remove_timestamps' =>
+      { :up   => 'add_timestamps :${1:table_name}$0',
+        :down => 'remove_timestamps :$1' },
 
-  'remove_add_timestamps' =>
-    { :up   => 'remove_timestamps :${1:table_name}$0',
-      :down => 'add_timestamps :$1' },
+    'remove_add_timestamps' =>
+      { :up   => 'remove_timestamps :${1:table_name}$0',
+        :down => 'add_timestamps :$1' },
 
-  'create_drop_table' =>
-    { :up   => 'create_table :${1:table_name}, :force => true do |t|
-  t.$0
-  t.timestamps
-end',
-      :down => 'drop_table :$1' },
+    'create_drop_table' =>
+      { :up   => 'create_table :${1:table_name}, :force => true do |t|
+    t.$0
+    t.timestamps
+  end',
+        :down => 'drop_table :$1' },
 
-  'change_change_table' =>
-    { :up   => 'change_table :${1:table_name} do |t|
-  t.$0
-end',
-      :down => 'change_table :$1 do |t|
-end' },
+    'change_change_table' =>
+      { :up   => 'change_table :${1:table_name} do |t|
+    t.$0
+  end',
+        :down => 'change_table :$1 do |t|
+  end' },
 
-  'add_remove_index' =>
-    { :up   => 'add_index :${1:table_name}, :${2:column_name}$0',
-      :down => 'remove_index :$1, :$2' },
+    'add_remove_index' =>
+      { :up   => 'add_index :${1:table_name}, :${2:column_name}$0',
+        :down => 'remove_index :$1, :$2' },
 
-  'add_remove_unique_index' =>
-    { :up   => 'add_index :${1:table_name}, ${2:[:${3:column_name}${4:, :${5:column_name}}]}, :unique => true$0',
-      :down => 'remove_index :$1, :column => $2' },
+    'add_remove_unique_index' =>
+      { :up   => 'add_index :${1:table_name}, ${2:[:${3:column_name}${4:, :${5:column_name}}]}, :unique => true$0',
+        :down => 'remove_index :$1, :column => $2' },
 
-  'add_remove_named_index' =>
-    { :up   => 'add_index :${1:table_name}, [:${2:column_name}${3:, :${4:column_name}}], :name => "${5:index_name}"${6:, :unique => true}$0',
-      :down => 'remove_index :$1, :name => :$5' }
-}
+    'add_remove_named_index' =>
+      { :up   => 'add_index :${1:table_name}, [:${2:column_name}${3:, :${4:column_name}}], :name => "${5:index_name}"${6:, :unique => true}$0',
+        :down => 'remove_index :$1, :name => :$5' }
+  }
 
-def indent(code)
-  lines = code.to_a.collect { |s| "\t\t" + s }
-  lines.to_s + "\n"
-end
-
-def insert_migration(snippet, text)
-  lines = text.to_a
-
-  up_code = indent(snippet[:up])
-  down_code = indent(snippet[:down])
-
-  # insert the self.up part of the snippet
-  lines[0] = up_code
-
-  # find the beginning of self.down and insert down code, this is hardly robust.
-  # assuming self.down is after self.up in the class
-  lines.each_with_index do |line, i|
-    if line =~ /^\s*def\s+self\.down\b/
-      lines[i, 1] = [lines[i], down_code]
-      break
-    end
+  def indent(code)
+    lines = code.to_a.collect { |s| "\t\t" + s }
+    lines.to_s + "\n"
   end
-  lines.to_s
+
+  def insert_migration(context, snippet, text)
+    lines = text.to_a
+
+    up_code = indent(snippet[:up])
+    down_code = indent(snippet[:down])
+
+    # insert the self.up part of the snippet
+    lines[0] = up_code
+
+    # FIX: find the beginning of self.down and insert down code, this is hardly robust.
+    # assuming self.down is after self.up in the class
+    lines.each_with_index do |line, i|
+      if line =~ /^\s*def\s+self\.down\b/
+        lines[i, 1] = [lines[i], down_code]
+        break
+      end
+    end
+    lines.to_s
+  end
+
+  def process_migration(context, snippet)
+    context.delete_word_left
+    # FIX: textmate bundle moves to BOL before going to end of file. Why?
+    context.move_to_beginning_of_line
+    context.move_to_end_of_document
+
+    # escape chars that are special in snippets
+    text = context.selection.gsub('[\$\`\\]', '\\\\\1')
+
+    if @@snippets.has_key? snippet
+      output = insert_migration(context, @@snippets[snippet], text)
+    else
+      # return the unmodified text
+      output = text
+    end
+    print output
+  end
+
 end
-
-snippet = ARGV.shift
-
-# escape chars that are special in snippets
-text = STDIN.read.gsub('[\$\`\\]', '\\\\\1')
-
-if snippets.has_key? snippet
-  output = insert_migration(snippets[snippet], text)
-else
-  # return the unmodified text
-  output = text
-end
-
-print output
--- commands/db_migration.rb
+++ commands/db_migration.rb
@@ -43,5 +43,175 @@ do |bundle|
   command "Redo Last Migration" do |cmd|
     cmd.invoke = "rake db:migrate:redo"
   end
+end
+
+# FIX: textmate macro has scopeType=local. what is our equivalent?
+with_defaults { :scope => "meta.rails.migration - meta.rails.migration.create_table - meta.rails.migration.change_table",
+                :input = :selection,
+                :output = :insert_as_snippet,
+                :output => :show_as_html }
+do |bundle|
+  command "Add / Remove Column" do |cmd|
+    # since a Command is a class definition, we can extend it using a module
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :add_remove_column)
+    end
+  end
+
+  command "Add / Remove Index" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mind"
+    
+    cmd.invoke do |context|
+      process_migration(context, :add_remove_index)
+    end
+  end
+
+  command "Add / Remove Named Index" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mind"
+    
+    cmd.invoke do |context|
+      process_migration(context, :add_remove_named_index)
+    end
+  end
+
+  command "Add / Remove Several Columns (marcc)" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "marcc"
+    
+    cmd.invoke do |context|
+      process_migration(context, :add_remove_column_continue)
+    end
+  end
+
+  command "Add / Remove Several Columns" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :add_remove_column_continue)
+    end
+  end
+
+  command "Add / Remove Timestamps" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :add_remove_timestamps)
+    end
+  end
+
+  command "Add / Remove Unique Index" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mind"
+    
+    cmd.invoke do |context|
+      process_migration(context, :add_remove_unique_index)
+    end
+  end
+
+  command "Change / Change Table" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mtab"
+    
+    cmd.invoke do |context|
+      process_migration(context, :change_change_table)
+    end
+  end
+
+  command "Change Column Default" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :change_column_default)
+    end
+  end
+
+  command "Change Column" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :change_column)
+    end
+  end
+
+  command "Create / Drop Table" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mtab"
+    
+    cmd.invoke do |context|
+      process_migration(context, :create_drop_table)
+    end
+  end
+
+  command "Remove / Add Timestamps" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :remove_add_timestamps)
+    end
+  end
+
+  command "Rename / Rename Several Columns (mncc)" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mncc"
+    
+    cmd.invoke do |context|
+      process_migration(context, :rename_column_continue)
+    end
+  end
+
+  command "Rename / Rename Several Columns" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :rename_column_continue)
+    end
+  end
+
+  command "Rename / Rename Column" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mcol"
+    
+    cmd.invoke do |context|
+      process_migration(context, :rename_column)
+    end
+  end
+
+  command "Rename / Rename Table" do |cmd|
+    extend IntelligentMigration
+
+    cmd.trigger = "mtab"
+    
+    cmd.invoke do |context|
+      process_migration(context, :rename_table)
+    end
+  end
 
 end
+
+# FIX: missing from textmate's plugin "Drop / Create Table" & "Remove / Add Column" macros
