e23ac01665cd9191826d25a1166326cc3d58edad
Andrew Shebanow <ashebanow@gmail.com>
2009-11-25 12:53:02 -0800

    convert find and has_ snippets

--- snippets/model_snippets.rb
+++ snippets/model_snippets.rb
@@ -3,6 +3,61 @@ require 'radrails'
 with_defaults { :scope => "source.ruby.rails" }
 do |bundle|
 
+  snippet "find(:all)" do |snippet|
+    snippet.trigger = "fina"
+    snippet.expansion = "find(:all${1:, :conditions =&gt; ['${2:${3:field} = ?}', ${5:true}]})"
+  end
+
+  snippet "find(:first)" do |snippet|
+    snippet.trigger = "finf"
+    snippet.expansion = "find(:first${1:, :conditions =&gt; ['${2:${3:field} = ?}', ${5:true}]})"
+  end
+
+  snippet "find(:last)" do |snippet|
+    snippet.trigger = "finl"
+    snippet.expansion = "find(:last${1:, :conditions =&gt; ['${2:${3:field} = ?}', ${5:true}]})"
+  end
+
+  snippet "find(id)" do |snippet|
+    snippet.trigger = "fini"
+    snippet.expansion = "find(${1:id})"
+  end
+
+  snippet "find_each" do |snippet|
+    snippet.trigger = "fine"
+    snippet.expansion = "find_each(${1::conditions =&gt; {:${2:field} =&gt; ${3:true}\}}) do |${4:${TM_CURRENT_WORD/(\w+)\./\L$1/g}}|\n\t\t$0\n\tend"
+  end
+
+  snippet "find_in_batches" do |snippet|
+    snippet.trigger = "finb"
+    snippet.expansion = "find_in_batches(${1::conditions =&gt; {:${2:field} =&gt; ${3:true}\}}) do |${4:${TM_CURRENT_WORD/(\w+)\./\L$1/g}}s|\n\t\t$4s.each do |$4|\n\t\t\t$0\n\t\tend\n\tend"
+  end
+
+  snippet "has_and_belongs_to_many" do |snippet|
+    snippet.trigger = "habtm"
+    snippet.expansion = "has_and_belongs_to_many :${1:object}${2:, :join_table => \"${3:table_name}\", :foreign_key => \"${4:${1}_id}\"}"
+  end
+
+  snippet "has_many" do |snippet|
+    snippet.trigger = "hm"
+    snippet.expansion = "has_many :${1:object}s${2:, :class_name => \"${1}\", :foreign_key => \"${4:reference}_id\"}"
+  end
+
+  snippet "has_many (through)" do |snippet|
+    snippet.trigger = "hmt"
+    snippet.expansion = "has_many :${1:objects}, :through => :${2:join_association}${3:, :source => :${4:${2}_table_foreign_key_to_${1}_table}}"
+  end
+
+  snippet "has_many :dependent => :destroy" do |snippet|
+    snippet.trigger = "hmd"
+    snippet.expansion = "has_many :${1:object}s${2:, :class_name => \"${1}\", :foreign_key => \"${4:reference}_id\"}, :dependent => :destroy$0"
+  end
+
+  snippet "has_one" do |snippet|
+    snippet.trigger = "ho"
+    snippet.expansion = "has_one :${1:object}${2:, :class_name => \"${3:${1/[[:alpha:]]+|(_)/(?1::\u$0)/g}}\", :foreign_key => \"${4:${1}_id}\"}"
+  end
+
   snippet "validates_acceptance_of if (vaoif)" do |snippet|
     snippet.trigger = "vaoif"
     snippet.expansion = "validates_acceptance_of :${1:terms}${2:${3:, :accept => '${4:1}'}${5:, :message => '${6:You must accept the terms of service}'}}, :if => proc { |obj| ${7:obj.condition?} }}"
