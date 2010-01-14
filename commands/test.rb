30d17974e00834d1f95b68fd8700213ae615ee9d
sgtcoolguy <cwilliams@aptana.com>
2010-01-14 12:47:09 -0500

    make commands work using terminal launches

--- commands/test.rb
+++ commands/test.rb
@@ -1,34 +1,35 @@
 require 'radrails'
+require 'radrails/terminal'
 
 with_defaults :scope => "source.ruby.rails, source.yaml",
               :key_binding => "CONTROL+\\",
-              :output => :show_as_html do |bundle|
+              :output => :discard do |bundle|
   command "Test All" do |cmd|
-    cmd.invoke = "rake test"
+    cmd.invoke {|context|  RadRails::Terminal.open("rake test", context.project.to_dir.path) }
   end
 
   command "Test Functionals" do |cmd|
-    cmd.invoke = "rake test:functionals"
+    cmd.invoke {|context|  RadRails::Terminal.open("rake test:functionals", context.project.to_dir.path) }
   end
 
   command "Test Integration" do |cmd|
-    cmd.invoke = "rake test:integration"
+    cmd.invoke {|context|  RadRails::Terminal.open("rake test:integration", context.project.to_dir.path) }
   end
 
   command "Test Plugins" do |cmd|
-    cmd.invoke = "rake test:plugins"
+    cmd.invoke {|context|  RadRails::Terminal.open("rake test:plugins", context.project.to_dir.path) }
   end
 
   command "Test Recent" do |cmd|
-    cmd.invoke = "rake test:recent"
+    cmd.invoke {|context|  RadRails::Terminal.open("rake test:recent", context.project.to_dir.path) }
   end
 
   command "Test Uncommitted" do |cmd|
-    cmd.invoke = "rake test:uncommitted"
+    cmd.invoke {|context|  RadRails::Terminal.open("rake test:uncommitted", context.project.to_dir.path) }
   end
 
   command "Test Units" do |cmd|
-    cmd.invoke = "rake test:units"
+    cmd.invoke {|context|  RadRails::Terminal.open("rake test:units", context.project.to_dir.path) }
   end
 
 end
