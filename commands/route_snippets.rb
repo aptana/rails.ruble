a12e9761f8dee8dc7e73ec59d0d89d86403b6894
sgtcoolguy <cwilliams@aptana.com>
2010-01-14 10:51:53 -0500

    add FIXME markers about unsupported snippet syntax

--- commands/route_snippets.rb
+++ commands/route_snippets.rb
@@ -29,26 +29,31 @@ with_defaults :scope => "text.html.ruby, text.haml" do |bundle|
 
   snippet "link_to (nested path plural)" do |snippet|
     snippet.trigger = "linpp"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "link_to ${1:\"${2:link text...}\"}, ${3:${10:parent}_${11:child}_path(${12:@}${13:${10}})}"
   end
 
   snippet "link_to (nested path)" do |snippet|
     snippet.trigger = "linp"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "link_to ${1:\"${2:link text...}\"}, ${3:${12:parent}_${13:child}_path(${14:@}${15:${12}}, ${16:@}${17:${13}})}"
   end
 
   snippet "link_to (path plural)" do |snippet|
     snippet.trigger = "lipp"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "link_to ${1:\"${2:link text...}\"}, ${3:${4:model}s_path}"
   end
 
   snippet "link_to (path)" do |snippet|
     snippet.trigger = "lip"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "link_to ${1:\"${2:link text...}\"}, ${3:${12:model}_path(${13:@}${14:${12}})}"
   end
 
   snippet "link_to model" do |snippet|
     snippet.trigger = "lim"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "link_to ${1:model}.${2:name}, ${3:${4:$1}_path(${14:$1})}"
   end
 
@@ -83,21 +88,25 @@ with_defaults :scope => "source.ruby.rails" do |bundle|
 
   snippet "redirect_to (nested path plural)" do |snippet|
     snippet.trigger = "renpp"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "redirect_to(${2:${10:parent}_${11:child}_path(${12:@}${13:${10}})})"
   end
 
   snippet "redirect_to (nested path)" do |snippet|
     snippet.trigger = "renp"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "redirect_to(${2:${12:parent}_${13:child}_path(${14:@}${15:${12}}, ${16:@}${17:${13}})})"
   end
 
   snippet "redirect_to (path plural)" do |snippet|
     snippet.trigger = "repp"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "redirect_to(${2:${10:model}s_path})"
   end
 
   snippet "redirect_to (path)" do |snippet|
     snippet.trigger = "rep"
+    # FIXME We don't support nested tab stops!
     snippet.expansion = "redirect_to(${2:${12:model}_path(${13:@}${14:${12}})})"
   end
 
@@ -117,21 +126,25 @@ with_defaults :scope => "meta.rails.routes" do |bundle|
 
   snippet "map.named_route" do |snippet|
     snippet.trigger = "map"
+    # FIXME Our snippet converter assumes '/' turns each portion into a selection from a menu right now...
     snippet.expansion = "${1:map}.${2:connect} '${3::controller/:action/:id}'"
   end
 
   snippet "map.resource" do |snippet|
     snippet.trigger = "mapr"
+    # FIXME We don't support tab stops higher than 9 and don't support a later one's default value being what was set in an earlier one (and not explicitly tying them togetehr as a pair)
     snippet.expansion = "${1:map}.resource :${2:resource}${10: do |${11:$2}|\n\t\t$0\n\tend}"
   end
 
   snippet "map.resources" do |snippet|
     snippet.trigger = "maprs"
+    # FIXME We don't support tab stops higher than 9 and don't support a later one's default value being what was set in an earlier one (and not explicitly tying them togetehr as a pair)
     snippet.expansion = "${1:map}.resources :${2:resource}${10: do |${11:$2}|\n\t\t$0\n\tend}"
   end
 
   snippet "map.with_options" do |snippet|
     snippet.trigger = "mapwo"
+    # FIXME We don't support a later one's default value being what was set in an earlier one (and not explicitly tying them togetehr as a pair)
     snippet.expansion = "${1:map}.with_options :${2:controller} => '${3:thing}' do |${4:$3}|\n\t\t$0\n\tend\n"
   end
 
