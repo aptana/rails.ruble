dad5b32012dfd6c28d105576c815ead39c6a0312
sgtcoolguy <cwilliams@aptana.com>
2010-01-14 12:16:18 -0500

    add FIXME markers for snippet syntax we don't support yet

--- commands/180_rails_snippets.rb
+++ commands/180_rails_snippets.rb
@@ -2,6 +2,7 @@ require 'radrails'
 
 snippet "form_tag" do |snippet|
   snippet.trigger = "ft"
+  # FIXME We don't support nested tab stops
   snippet.expansion = "form_tag(${1::action => \"${5:update}\"}${6:, {:${8:class} => \"${9:form}\"}}) do\n\t$0\n"
   snippet.scope = [ :text_html_ruby, :text_haml ]
 end
