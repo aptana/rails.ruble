0d1f9b5b309f9d05a8a8e12221442f3a6649ddfb
sgtcoolguy <cwilliams@aptana.com>
2010-01-18 15:22:21 -0500

    minor cleanup

--- commands/documentation_for_word.rb
+++ commands/documentation_for_word.rb
@@ -1,15 +1,15 @@
 require 'radrails'
-# FIXME Scope matching is borken so this never seems to be enabled
+
 command "Documentation for Word" do |cmd|
   cmd.key_binding = "CONTROL+H"
   # if selection isn't empty, use it, otherwise use the word containing the current selection
-  cmd.input = [ :selection, :word ]
+  cmd.input = :selection, :word
   cmd.output = :show_as_html
   cmd.scope = "source.ruby.rails, text.html.ruby, text.haml"
   cmd.invoke do |context|
-    # context.in will figure out what to pass in correctly based on command settings
-    url = "http://apidock.com/rails/search/quick?query=" + context.in.read
-
+    word = context.input.read
+    # context.input will figure out what to pass in correctly based on command settings
+    url = "http://apidock.com/rails/search/quick?query=" + word
     # this is a hack - relies on embedded html view fetching page for you.
     "<meta http-equiv='Refresh' content='0;URL=#{url}'>"
   end
