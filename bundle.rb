768849fd4b6a2905fe3a7aedf8e1136992ce5880
sgtcoolguy <cwilliams@aptana.com>
2010-02-12 14:05:46 -0500

    use the new file_types syntax

--- bundle.rb
+++ bundle.rb
@@ -15,7 +15,7 @@ based heavily on the original SyncPeople bundle.
 END
 
   bundle.repository = "git://github.com/aptana/rails.ruble.git"
-  bundle.register_file_type("*.rb", "source.ruby.rails")
+  bundle.file_types['source.ruby.rails'] = "*.rb"
 
   # most commands install into a dedicated rails menu
   # See also the alternative, HAML-style syntax in menu.rrmenu
