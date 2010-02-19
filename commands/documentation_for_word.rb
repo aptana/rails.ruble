21904d83c77fa787bb441be3a0a4a54514968744
Andrew Shebanow <ashebanow@gmail.com>
2010-02-19 11:01:33 -0800

    minor tweaks to Documentation for Word

--- commands/documentation_for_word.rb
+++ commands/documentation_for_word.rb
@@ -2,15 +2,14 @@ require 'ruble'
 
 command "Documentation for Word" do |cmd|
   cmd.key_binding = "CONTROL+H"
-  # if selection isn't empty, use it, otherwise use the word containing the current selection
-  cmd.input = :selection, :word
-  cmd.output = :show_as_html
   cmd.scope = "source.ruby.rails, text.html.ruby, text.haml"
+  cmd.input = :selection, :word
+
+  # this is a hack - relies on embedded html view fetching page for you.
+  cmd.output = :show_as_html
   cmd.invoke do |context|
     word = STDIN.read
-    # context.input will figure out what to pass in correctly based on command settings
     url = "http://apidock.com/rails/search/quick?query=" + word
-    # this is a hack - relies on embedded html view fetching page for you.
     "<meta http-equiv='Refresh' content='0;URL=#{url}'>"
   end
 end
