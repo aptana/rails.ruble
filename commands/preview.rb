0f399464a08a4a5c31c18279f9980020eec4fae2
sgtcoolguy <cwilliams@aptana.com>
2010-03-23 11:18:51 -0400

    handle nil TM_FILEPATH (which is what we get when run from new gear menu preview item without any editors open)

--- commands/preview.rb
+++ commands/preview.rb
@@ -8,32 +8,34 @@ command "Preview" do |cmd|
   cmd.invoke do |context|
     default_id = 1 # Default resource id to use
     url = context.project.root_url
-    project_relative_path = ENV['TM_FILEPATH'].sub(/^#{Regexp.escape(ENV['TM_PROJECT_DIRECTORY'])}\//, '')
-    # Grab mapper like "rake routes" does and then reverse engineer the controller and action into an URL!
-    require 'rails/rails_path'
-    path = RailsPath.new(project_relative_path)
-    # Actually introspect routes from the rails app!
-    Dir.chdir(ENV['TM_PROJECT_DIRECTORY'])
-    result = ''
-    cmd_line = "ruby \"#{ENV['TM_BUNDLE_SUPPORT']}/bin/rails3_routes.rb\" \"#{ENV['TM_PROJECT_DIRECTORY']}\" #{path.controller_name} #{path.action_name}"
-    IO.popen(cmd_line) {|io| result << io.read }
-    if result.length == 0 # Something went hinky, couldn't introspect routes
-      # The "dumb" way...
-      url += path.controller_name
-      case path.action_name.downcase.to_sym
-      when :show, :update, :destroy
-        url += "/" + default_id
-      when :edit
-        url += "/#{default_id}/edit"
-      when :index, :create, :list
-        url # No changes, just hit controller plural name
-      else # :new, 
-        url += "/#{path.action_name}"
-      end       
-    else
-      result = result.strip.gsub(/\(.+\)/, '').gsub(/:id/, default_id.to_s)
-      url += result 
-    end    
+    if ENV['TM_FILEPATH']
+      project_relative_path = ENV['TM_FILEPATH'].sub(/^#{Regexp.escape(ENV['TM_PROJECT_DIRECTORY'])}\//, '')
+      # Grab mapper like "rake routes" does and then reverse engineer the controller and action into an URL!
+      require 'rails/rails_path'
+      path = RailsPath.new(project_relative_path)
+      # FIXME If path is not controller/view/model/helper/unit test/etc type, just assume preview root!
+      # Actually introspect routes from the rails app!
+      Dir.chdir(ENV['TM_PROJECT_DIRECTORY'])
+      cmd_line = "ruby \"#{ENV['TM_BUNDLE_SUPPORT']}/bin/rails3_routes.rb\" \"#{ENV['TM_PROJECT_DIRECTORY']}\" #{path.controller_name} #{path.action_name || 'index'}"
+      result = IO.popen(cmd_line) {|io| io.read }
+      if result.length == 0 # Something went hinky, couldn't introspect routes
+        # The "dumb" way...
+        url += path.controller_name
+        case (path.action_name || 'index').downcase.to_sym
+        when :show, :update, :destroy
+          url += "/" + default_id
+        when :edit
+          url += "/#{default_id}/edit"
+        when :index, :create, :list
+          url # No changes, just hit controller plural name
+        else # :new, 
+          url += "/#{path.action_name}"
+        end       
+      else
+        result = result.strip.gsub(/\(.+\)/, '').gsub(/:id/, default_id.to_s)
+        url += result 
+      end
+    end
     url = url.gsub(/\/\//, '/') # Replace // with /
     context.browser.open(url, :browser => :default)   
   end
